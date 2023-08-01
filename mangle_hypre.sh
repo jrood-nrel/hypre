4f675a8617e20618bc7cd2192afe254221e576ff
Jon Rood <jon.rood@nrel.gov>
2023-08-01 11:29:52 -0600

    Fix files.

--- mangle_hypre.sh
+++ mangle_hypre.sh
@@ -19,3 +19,6 @@ find src -type f -name '*hypre*' -exec bash -c 'git mv $0 ${0/hypre/nalu_hypre}'
 find src -type f -name '*HYPRE*' -exec bash -c 'git mv $0 ${0/HYPRE/NALU_HYPRE}' {} \;
 git mv src/NALU_NALU_HYPRE.h src/NALU_HYPRE.h
 git mv src/NALU_NALU_HYPREf.h src/NALU_HYPREf.h
+find src -type f -exec sed -i.bak 's/NALU_NALU/NALU/g' {} \;
+find . -name \*.bak | xargs rm
+git clean -df
